45a096ce703bcd6168c9c4ea7d813211afa5202c
Dmitry Ketov <dketov@gmail.com>
2011-11-14 11:54:01 +0400

    Moodle Plugin database requirements

--- version.php
+++ version.php
@@ -1,4 +1,8 @@
 <?php
 
 $plugin->version = 2011111300;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.0.1';
+$plugin->requires  = 201107010; // Was (honestly) tested on 2011070102.02
+
 ?>
