a283594ff47535df5c4df44d8566d81cac0f5c2a
Dmitry Ketov <dketov@gmail.com>
2011-11-28 00:50:31 +0400

    Another $plugin->requires fix

--- version.php
+++ version.php
@@ -3,6 +3,6 @@
 $plugin->version = 2011111300;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.0.1';
-$plugin->requires  = 20110701000; // Was (honestly) tested on 2011070102.02
+$plugin->requires  = 2011070100; // Was (honestly) tested on 2011070102.02
 
 ?>
