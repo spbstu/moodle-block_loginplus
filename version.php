3fb1abf18e8ea58b59a9c86de7707695f8a20d00
Dmitry Ketov <dketov@gmail.com>
2011-11-28 01:18:34 +0400

    %)

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin->version = 2011111300;
+$plugin->version = 2011112800;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.0.1';
 $plugin->requires  = 2011070100; // Was (honestly) tested on 2011070102.02
